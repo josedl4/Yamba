5b0fb33bfcab200e46bc29e20c538fc329e2fccb
Jose L Martin Martin <joseop21@gmail.com>
2017-11-05 13:47:42 +0100

    WIP: Trabajando en la implementación del tema 7

---
+++ app/src/main/java/com/example/joselm/yambaandroidtestjl/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.joselm.yambaandroidtestjl;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Comprobar que no se ha creado la actividad antes
+        if (savedInstanceState == null) {
+            // Crear el fragment 
+            SettingsFragment fragment = new SettingsFragment();
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment,
+                            fragment.getClass().getSimpleName())
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/joselm/yambaandroidtestjl/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.example.joselm.yambaandroidtestjl;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+public class SettingsFragment extends PreferenceFragment {
+    private SharedPreferences prefs;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    }
+}
